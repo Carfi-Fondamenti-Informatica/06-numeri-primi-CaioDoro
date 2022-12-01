c1f8cc2601cb828f30acbb6794309d651e6486e0
CaioDoro <117940103+CaioDoro@users.noreply.github.com>
2022-12-01 12:02:40 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,11 @@
 #include <iostream>
+#include "lib.h"
 using namespace std;
 
-int NP (int num) {
-    int i;
-    int div=0;
-    //div= numero de divisores de num
-    for (i = 1; i <= num; i++)
 
-    if (num%i==0) {
-        div++;
-    }
-    return div;}
 
    int main() {
        int num;
-       bool a;
        cin >> num;
        if (NP(num) == 2) {
            cout << "numero primo" << endl;
