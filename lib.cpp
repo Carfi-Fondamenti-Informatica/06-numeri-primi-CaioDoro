480ceb8856f39dd3e123d2940da68dad78cdb35a
CaioDoro <117940103+CaioDoro@users.noreply.github.com>
2022-12-01 11:44:40 -0300

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,10 @@
+int NP (int num) {
+    int i;
+    int div=0;
+    //div= numero de divisores de num
+    for (i = 1; i <= num; i++)
 
+    if (num%i==0) {
+        div++;
+    }
+    return div;}
